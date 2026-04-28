75ec92955792d2229145fa175cf5c8c720614dda
Hsi <its.hsichen@gmail.com>
2026-04-28 15:50:34 +0800

    chore(lint): enforce no-require-imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -29,7 +29,6 @@ export default defineConfig(
       "@typescript-eslint/no-deprecated": "off",
       "@typescript-eslint/no-empty-function": "off",
       "@typescript-eslint/no-misused-promises": "off",
-      "@typescript-eslint/no-require-imports": "off",
       "@typescript-eslint/no-shadow": "off",
       "@typescript-eslint/no-base-to-string": "off",
       "@typescript-eslint/no-confusing-void-expression": "off",
